20e522ffd9bffc73149ef4f880dfa11af9f56b22
josuepaniagua <josue.paniagua.jap@gmail.com>
2022-10-15 17:43:28 -0500

    added roles to lib folder, added tests for roles, and added style sheet to dist folder

---
+++ index.js
@@ -0,0 +1,7 @@
+var inquirer = require("inquirer");
+var fs = require("fs");
+var executeTemplate = require("./dist/template");
+var Intern = require("./lib/intern");
+var Engineer = require("./lib/engineer");
+var Manager = require("./lib/manager");
+
